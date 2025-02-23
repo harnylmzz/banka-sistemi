34836cedd776b4b63af21b3076518d088979c190
harnylmzz <harnylmzzonline@gmail.com>
2025-02-23 03:39:17 +0300

    repositories

--- BankingCreditSystem.Core/Repositories/Entity.cs
+++ BankingCreditSystem.Core/Repositories/Entity.cs
@@ -1,7 +1,7 @@
 public abstract class Entity<TId>
 {
     public TId Id { get; set; } = default!;
-    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedDate { get; set; } 
     public DateTime? UpdatedDate { get; set; }
     public DateTime? DeletedDate { get; set; }
 
